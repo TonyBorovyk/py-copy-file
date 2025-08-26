439deec0e30075bf381f2bb4eb339b968afe76a5
TonyBor <antonbovik@gmail.com>
2025-08-26 16:43:57 +0300

    fix exception

--- app/main.py
+++ app/main.py
@@ -1,14 +1,15 @@
+import os
+
+
 def copy_file(command_files: str) -> None:
-    try:
-        count_variable = command_files.split()
-        command, file_name_1, file_name_2 = count_variable
-        if command == "cp" and f"{file_name_1}" != f"{file_name_2}":
+    parts = command_files.split()
+    if len(parts) == 3 and parts[0] == "cp":
+
+        command, file_name_1, file_name_2 = parts
+        if (os.path.isfile(file_name_1)
+                and f"{file_name_1}" != f"{file_name_2}"):
+
             with (open(file_name_1, "r") as file_in,
-                  open(file_name_2, "w") as file_out):
+                    open(file_name_2, "w") as file_out):
+
                 file_out.write(file_in.read())
-
-    except FileNotFoundError:
-        print("file not found")
-
-    except ValueError:
-        print("not enough values")
