c72bf4b2f63a3a828e41b710b6e1dd76d9c0cb4e
TonyBor <antonbovik@gmail.com>
2025-08-26 17:04:20 +0300

    fix file check

--- app/main.py
+++ app/main.py
@@ -1,15 +1,15 @@
-import os
-
-
 def copy_file(command_files: str) -> None:
     parts = command_files.split()
-    if len(parts) == 3 and parts[0] == "cp":
+    if len(parts) != 3 or parts[0] != "cp":
+        return
 
-        command, file_name_1, file_name_2 = parts
-        if (os.path.isfile(file_name_1)
-                and f"{file_name_1}" != f"{file_name_2}"):
+    file_name_1, file_name_2 = parts[1], parts[2]
+    if f"{file_name_1}" == f"{file_name_2}":
+        return
 
-            with (open(file_name_1, "r") as file_in,
-                    open(file_name_2, "w") as file_out):
-
-                file_out.write(file_in.read())
+    try:
+        with (open(file_name_1, "r") as file_in,
+              open(file_name_2, "w") as file_out):
+            file_out.write(file_in.read())
+    except FileNotFoundError:
+        pass
